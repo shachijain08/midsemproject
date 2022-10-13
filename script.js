03f5a49450cc02ac3cbb077a50a4204110de85d8
shachijain08 <shachi.jain2003@gmail.com>
2022-10-13 13:05:15 -0400

    create variables in js file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+let id = 1;
+let characters = [];
+let flag = true;
+let height, weight, hp, attack, defense, specAttack, specDefense, spped;
+let moves = [];
+const pokeName = document.getElementById("name");
+const pokeImg = document.getElementById("pokeImg");
+const leftButton = document.getElementsByClassName("button")[0];
+const rightButton = document.getElementsByClassName("button")[1];
+const list = document.getElementById("list");
+const infoOutput = document.getElementById("infoOutput");
+const info = document.getElementById("info");
+const moveButton = document.getElementById("moves");
+
